c09ca2ff4e43220f9f615f5e60d88a4bd52998d0
Garamida abderraouf adel <abderraouf.adel.garamida@ensia.edu.dz.com>
2025-11-18 22:54:19 +0100

    feat : structure of project

---
+++ frontend/src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function AppRouter() {
+           {/*Put all your routers here */}
+  return (
+    <Router>
+      <Routes>
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
