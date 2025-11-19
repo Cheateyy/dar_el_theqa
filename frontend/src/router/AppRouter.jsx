48fcb164b26a22053c653a7aac5e1ef5d1c6d456
abdelaliMokhati <abdelali.mokhati@ensia.edu.dz>
2025-11-19 12:06:48 +0100

    feat: add route and assets from the design

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-  {/*Put all your routers here */}
+import Home from '../pages/buyer/components/Home';
+{/*Put all your routers here */ }
 function AppRouter() {
-           
+
   return (
     <Router>
       <Routes>
-        
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   );
