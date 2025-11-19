a329952d5efb5a16c8912c572ed4530da7a8da81
abdelaliMokhati <abdelali.mokhati@ensia.edu.dz>
2025-11-19 17:20:56 +0100

    feat: add header

--- frontend/src/pages/buyer/components/BuyerHome.jsx
+++ frontend/src/pages/buyer/components/BuyerHome.jsx
@@ -1,14 +1,20 @@
 import logo from "@/assets/images/logo.png"
 import { Button } from "@/components/ui/button"
+import { GlobeIcon } from "lucide-react"
 
 export default function BuyerHome() {
     return (
         <header>
-            <div className="flex flex-row justify-between">
-                <img src={logo} alt="logo" />
-                <div>
-                    <Button>HI</Button>
-                    <Button>Hello</Button>
+            <div className="flex flex-row px-16 justify-between">
+                <div className="relative -left-10">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="flex flex-row items-center">
+                    <Button className="w-28 h-12 mr-3 text-gray-600" variant={'ghost'}>
+                        <GlobeIcon />
+                        Language
+                    </Button>
+                    <Button className="w-38 h-12 p-3">Log In</Button>
                 </div>
             </div>
         </header>
