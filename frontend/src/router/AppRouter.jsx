efc6dcb261cd7c3dad9dd0ae778619a7252901a1
GARAMIDA abderraoufe adel <30668902+adel66@users.noreply.github.com>
2025-11-19 10:21:02 +0100

    Feature/garamida abderraouf adel (#2)
    * feat : structure of project
    
    * feat : structure of project
    
    * feat : structure of project
    
    ---------
    
    Co-authored-by: Garamida abderraouf adel <abderraouf.adel.garamida@ensia.edu.dz.com>

---
+++ frontend/src/router/AppRouter.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+  {/*Put all your routers here */}
+function AppRouter() {
+           
+  return (
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
