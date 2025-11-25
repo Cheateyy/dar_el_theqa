a4e9dac21231059c57a98435a4f942256f736988
abdelaliMokhati <abdelali.mokhati@ensia.edu.dz>
2025-11-25 17:02:44 +0100

    fix: delete old landingPage (now it is under src/pages/buyer folder)

--- frontend/src/pages/landingPage/components/PartnerCard.jsx
+++
@@ -1,9 +0,0 @@
-import partnerImg from "../assets/partner_card.jpg";
-
-export function PartnerCard() {
-    return (
-        <div className="rounded-2xl shadow-xl">
-            <img src={partnerImg} alt="partner card" />
-        </div>
-    )
-}
